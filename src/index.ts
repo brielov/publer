a63ee0cb82c7c54e21cedcde005ca0c8075843a0
Gabriel Vaquer <brielov@icloud.com>
2023-06-29 15:26:42 -0300

    Expose PubSub type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-export const publer = <T extends { [key: string]: any[] }>() => {
+type EventBus = { [key: string]: any[] };
+
+export type PubSub<T extends EventBus> = [
+  pub: <K extends keyof T>(eventName: K, ...args: T[K]) => void,
+  sub: <K extends keyof T>(
+    eventName: K,
+    listener: (...args: T[K]) => void,
+  ) => void,
+];
+
+export const publer = <T extends EventBus>(): PubSub<T> => {
   const eventMap = new Map<keyof T, Set<(...args: any) => void>>();
   return [
     <K extends keyof T>(eventName: K, ...args: T[K]) =>
-      eventMap.get(eventName)?.forEach((observer) => observer(...args)),
-    <K extends keyof T>(eventName: K, observer: (...args: T[K]) => void) =>
-      (eventMap.get(eventName)?.add(observer) ||
-        eventMap.set(eventName, new Set([observer]))) &&
-      (() => void eventMap.get(eventName)?.delete(observer)),
-  ] as const;
+      eventMap.get(eventName)?.forEach((fn) => fn(...args)),
+    <K extends keyof T>(eventName: K, listener: (...args: T[K]) => void) =>
+      (eventMap.get(eventName)?.add(listener) ||
+        eventMap.set(eventName, new Set([listener]))) &&
+      (() => void eventMap.get(eventName)?.delete(listener)),
+  ];
 };
